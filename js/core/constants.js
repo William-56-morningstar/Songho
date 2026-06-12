75ec4ea688c3dea4095d17ada8462e97383cce89
William-56-morningstar <w.illiamaloys56@gmail.com>
2026-06-12 02:29:47 +0100

    Add constants for game rules and ending policy

---
+++ js/core/constants.js
@@ -0,0 +1,14 @@
+// js/core/constants.js
+
+const RULES = {
+  players: ["north", "south"],
+  pitsPerPlayer: 7,
+  initialSeedsPerPit: 5,
+  totalSeeds: 70,
+  victoryScore: 40,
+  lowBoardLimit: 10,
+  captureValues: [2, 3, 4],
+  maxNormalSowSeeds: 13
+};
+
+const ENDING_POLICY = "strict_40_or_draw";
